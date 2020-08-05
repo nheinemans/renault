484b7668231a6cf83b9956db716fcee6aede1a76
Nick Heinemans <nick@heinemans.net>
2020-08-05 21:29:49 +0200

    Adding some ZE40 metrics and optimized metric parsing in script

--- renault.sh
+++ renault.sh
@@ -4,6 +4,7 @@
 # Download https://github.com/jamesremuscat/pyze?fbclid=IwAR1KDSOIib6sIaQ1cGDUGKeNnceU2Kg1QICp1xUetomR6jFjONkjp1--CAc to /home/renault/
 # Make a cronjob that runs every 5 minutes for /home/renault/renault.sh
 
+echo "INFO: Looking up vehicle with registration $REG in your Renault account."
 VIN=`pyze vehicles | grep $REG | awk {'print $NF'} | sed "s/.//;s/.$//"`
 if [[ $? -ne 0 ]]; then
   echo "ERROR: The Renault API is not working properly, could not get VIN number for vehicle $REG."
@@ -16,7 +17,7 @@ curl="/usr/bin/curl"
 FILE="/renault.txt"
 
 # Get metrics from Renault API into temp file
-echo "The renault.sh script has started and the data is being retrieved from Renault for registration $vehicle with VIN $VIN"
+echo "INFO: Retrieving data from Renault for vehicle $vehicle with VIN $VIN"
 /usr/local/bin/pyze status --vin $VIN --km > $FILE
 if [[ $? -ne 0 ]]; then
   echo "ERROR: The Renault API is not working properly, no metrics received."
@@ -24,33 +25,37 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # Battery related variables
-batpercent=`cat $FILE | grep level | awk '{print $3}' | rev | cut -c2- | rev`
-echo "Battery percentage: $batpercent"
-energy=`cat $FILE | grep energy | awk '{print $3}' | rev | cut -c4- | rev`
-echo "Energy available (kWh): $energy"
-rangekm=`cat $FILE | grep estim | awk '{print $3}' | rev | cut -c3- | rev`
-echo "Range (km): $rangekm"
+batpercent=`cat $FILE | grep "^Battery level" | sed 's/[^0-9.]*//g'`
+echo "INFO: Battery percentage: $batpercent"
+energy=`cat $FILE | grep "^Available energy" | sed 's/[^0-9.]*//g'`
+echo "INFO: Energy available (kWh): $energy"
+rangekm=`cat $FILE | grep "^Range estimate" | sed 's/[^0-9.]*//g'`
+echo "INFO: Range (km): $rangekm"
 
 # Temperature related variables
-battemp=`cat $FILE | grep "Battery temp" | awk '{print $3}' | rev | cut -c4- | rev`
-echo "Battery temperature: $battemp"
-exttemp=`cat $FILE | grep "External temp" | awk '{print $3}' | rev | cut -c4- | rev`
-echo "External temperature: $exttemp"
+battemp=`cat $FILE | grep "^Battery temperature" | sed 's/[^0-9.]*//g'`
+echo "INFO: Battery temperature: $battemp"
+exttemp=`cat $FILE | grep "^External temperature" | sed 's/[^0-9.]*//g'`
+echo "INFO: External temperature: $exttemp"
 
 # Charging related variables
 charging=`cat $FILE | grep "Charging state" | awk '{print $3}'`
-echo "Charging state: $charging"
+echo "INFO: Charging state: $charging"
 pluggedin=`cat $FILE | grep "Plug state" | awk '{print $3}'`
-echo "Plugged state: $pluggedin"
-remainingtime=`cat $FILE | grep remaining | awk '{print $3}' | awk -F: {'print $1*3600+$2*60'} `
-echo "Remaining charging time (seconds): $remainingtime"
+echo "INFO: Plugged state: $pluggedin"
+remainingtime=`cat $FILE | grep "^Time remaining" | awk '{print $3}' | awk -F: {'print $1*3600+$2*60'} `
+echo "INFO: Remaining charging time (seconds): $remainingtime"
+chargerate=`cat $FILE | grep "^Charge rate" | sed 's/[^0-9.]*//g'`
+echo "INFO: Charge rate (kW): $chargerate"
 
 # Car related variables
 gpslat=`cat $FILE | grep Location | awk '{print $2}' | awk -F, {'print $1'} `
 gpslong=`cat $FILE | grep Location | awk '{print $2}' | awk -F, {'print $2'} `
-echo "GPS location (long/lat): $gpslat,$gpslong"
-mileage=`cat $FILE | grep mileage | awk '{print $3}' | rev | cut -c3- | rev`
-echo "Milage (km): $mileage"
+echo "INFO: GPS location (long/lat): $gpslat,$gpslong"
+mileage=`cat $FILE | grep "^Total mileage" | sed 's/[^0-9.]*//g'`
+echo "INFO: Mileage (km): $mileage"
+acstate=`cat $FILE | grep "AC state" | awk '{print $3}'`
+echo "INFO: AC state: $acstate"
 
 # Battery related variables to Influxdb
 if [[ $batpercent =~ [0-9] ]];then
@@ -99,6 +104,11 @@ if [[ $remainingtime =~ [0-9] ]];then
 else
   echo "WARN: Remaining time digit not found. No syncronization to InfluxDB."
 fi
+if [[ $chargerate =~ [0-9] ]];then
+  $curl -XPOST http://$IP:$PORT/write?db=renault --data-binary "charging,vehicle=$vehicle chargerate=$chargerate"
+else
+  echo "WARN: Charge rate digit not found. No syncronization to InfluxDB."
+fi
 
 # Vehicle related variables to Influxdb
 if [[ $gpslat =~ [0-9] ]];then
@@ -110,4 +120,10 @@ if [[ $mileage =~ [0-9] ]];then
   $curl -XPOST http://$IP:$PORT/write?db=renault --data-binary "car,vehicle=$vehicle milage=$mileage"
 else
   echo "WARN: Milage not found. No syncronization to InfluxDB."
+fi
+if [[ $acstate = "Off" ]]
+then
+  $curl -XPOST http://$IP:$PORT/write?db=renault --data-binary "car,vehicle=$vehicle acstate=0"
+else
+  $curl -XPOST http://$IP:$PORT/write?db=renault --data-binary "car,vehicle=$vehicle acstate=1"
 fi
